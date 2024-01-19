ada04e9bfb925e00c6e4d1f903e4b28cb57eecc7
crishof <cristianhof@gmail.com>
2024-01-19 17:00:00 +0100

    add product-sv

---
+++ product-sv/src/main/java/com/crishof/productsv/ProductSvApplication.java
@@ -0,0 +1,13 @@
+package com.crishof.productsv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductSvApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductSvApplication.class, args);
+    }
+
+}
---
+++ product-sv/src/test/java/com/crishof/productsv/ProductSvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crishof.productsv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductSvApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
