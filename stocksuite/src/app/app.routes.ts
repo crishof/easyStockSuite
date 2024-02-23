621809012fe16c1243b7e7e9fc90ec25593cf2e0
Cristian Hoffmann <cristianhof@gmail.com>
2024-02-23 19:53:01 +0100

    add navbar components

--- stocksuite/src/app/app.routes.ts
+++ stocksuite/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ProductsComponent } from './pages/products/products.component';
+import { ProductDetailsComponent } from './pages/product-details/product-details.component';
+import { BrandComponent } from './pages/brand/brand.component';
+import { CategoryComponent } from './pages/category/category.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'products', component: ProductsComponent },
+  { path: 'products/:id', component: ProductDetailsComponent },
+  { path: 'brand', component: BrandComponent },
+  { path: 'category', component: CategoryComponent },
+  { path: '**', redirectTo: '', pathMatch: 'full' },
+];
