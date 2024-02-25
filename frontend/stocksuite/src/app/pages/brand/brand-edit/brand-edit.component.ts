834d3b68291b28cee22dae94b59f88ac4e9f5ac9
Cristian Hoffmann <cristianhof@gmail.com>
2024-02-25 11:42:12 +0100

    add edit brand

--- frontend/stocksuite/src/app/pages/brand/brand-edit/brand-edit.component.ts
+++ frontend/stocksuite/src/app/pages/brand/brand-edit/brand-edit.component.ts
@@ -1,14 +1,64 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
+import { CommonModule, NgClass } from '@angular/common';
 import { IBrand } from '../../../model/brand.model';
+import { Injectable, inject } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { BrandService } from '../../../services/brand.service';
 
 @Component({
   selector: 'app-brand-edit',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule, NgClass],
   templateUrl: './brand-edit.component.html',
   styleUrl: './brand-edit.component.css',
 })
-export class BrandEditComponent {
-  public brand?: IBrand;
+export class BrandEditComponent implements OnInit {
+  @Input() brand: IBrand | undefined;
+  @Output() onSave = new EventEmitter<IBrand>();
+
+  brandForm!: FormGroup;
+  brandService = inject(BrandService);
+
+  constructor(private formBuilder: FormBuilder) {
+    this.brandForm = formBuilder.group({
+      brandName: ['', [Validators.required]],
+    });
+  }
+
+  enviar(event: Event) {
+    event.preventDefault();
+
+    const updatedBrand: IBrand = {
+      id: this.brand?.id || '',
+      name: this.brandForm.get('brandName')?.value || '',
+      // Agregar otros campos de edicion
+    };
+
+    console.log('Id antes de la actualización:', this.brand?.id);
+    console.log('Brand antes de la actualización:', this.brand);
+
+    this.brandService.updateBrand(updatedBrand.id, updatedBrand).subscribe(
+      (response) => {
+        console.log('Brand actualizada:', response);
+        this.onSave.emit(response);
+      },
+      (error) => {
+        console.log('Error al actualizar:', error);
+      }
+    );
+  }
+
+  ngOnInit(): void {}
+
+  hasErrors(field: string, typeError: string) {
+    return (
+      this.brandForm.get(field)?.hasError(typeError) &&
+      this.brandForm.get(field)?.touched
+    );
+  }
 }
