7f0863fdb1b67d1b2843e8e30673a4e60f6eccb7
crishof <cristianhof@gmail.com>
2024-05-02 23:15:51 +0200

    supplier navbar component

--- frontend/stocksuite/src/app/pages/supplier/supplier-navbar/supplier-navbar.component.ts
+++ frontend/stocksuite/src/app/pages/supplier/supplier-navbar/supplier-navbar.component.ts
@@ -1,9 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-supplier-navbar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterLink],
   templateUrl: './supplier-navbar.component.html',
   styleUrl: './supplier-navbar.component.css'
 })
