01e2be5849ca843631b27a9b860003dfe39060d6
crishof <cristianhof@gmail.com>
2024-05-06 23:58:23 +0200

    supplier invoice form

--- frontend/stocksuite/src/app/pages/supplier/supplier-invoice/supplier-invoice.component.ts
+++ frontend/stocksuite/src/app/pages/supplier/supplier-invoice/supplier-invoice.component.ts
@@ -1,13 +1,29 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatNativeDateModule } from '@angular/material/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-supplier-invoice',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MatDatepickerModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatNativeDateModule,
+  ],
   templateUrl: './supplier-invoice.component.html',
   styleUrl: './supplier-invoice.component.css',
 })
 export class SupplierInvoiceComponent implements OnInit {
-  ngOnInit(): void {}
+  dateControl = new FormControl(new Date());
+
+  ngOnInit(): void {
+    this.dateControl.setValue(new Date());
+  }
 }
