9d6ad96c9685becf9b80e527afac33f506a4920d
crishof <cristianhof@gmail.com>
2024-05-09 11:29:18 +0200

    add item component

---
+++ frontend/stocksuite/src/app/pages/supplier/supplier-invoice/invoice-items/invoice-items.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-invoice-items',
+  standalone: true,
+  imports: [],
+  templateUrl: './invoice-items.component.html',
+  styleUrl: './invoice-items.component.css'
+})
+export class InvoiceItemsComponent {
+
+}
