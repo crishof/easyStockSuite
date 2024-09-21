5169c37a2e69e83c02125081760d6dc90ab5eae9
crishof <cristianhof@gmail.com>
2024-09-21 09:47:24 +0200

    add transaction service

---
+++ microservices/transaction-sv/src/main/java/com/crishof/transactionsv/TransactionSvApplication.java
@@ -0,0 +1,13 @@
+package com.crishof.transactionsv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionSvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionSvApplication.class, args);
+	}
+
+}
