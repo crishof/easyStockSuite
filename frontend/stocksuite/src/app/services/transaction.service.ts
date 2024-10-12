f7d54039ec08573934002470a1899a431de16713
crishof <cristianhof@gmail.com>
2024-10-13 00:40:57 +0200

    readonly

--- frontend/stocksuite/src/app/services/transaction.service.ts
+++ frontend/stocksuite/src/app/services/transaction.service.ts
@@ -1,19 +1,25 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { ITransaction } from '../model/transaction.model';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TransactionService {
-  private _http = inject(HttpClient);
+  readonly _http = inject(HttpClient);
 
-  private _urlBase = 'http://localhost:443/transaction-sv/transaction';
+  readonly _urlBase = 'http://localhost:443/transaction-sv/transaction';
 
   getTransactions(id: string): Observable<ITransaction[]> {
     return this._http.get<ITransaction[]>(
       `${this._urlBase}/getAllBySupplier/${id}`
     );
+  }
+
+  readonly transactionSource = new BehaviorSubject<ITransaction | null>(null);
+
+  setSelectedTransaction(transaction: ITransaction) {
+    this.transactionSource.next(transaction);
   }
 }
